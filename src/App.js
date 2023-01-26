6819a73b3241fed11b907e535499932fc5b31783
yng-blood <mfstarboey@gmail.com>
2023-01-26 11:35:58 +0530

    eract2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+export default class App extends Component {
+  state={
+    Person:" ",
+    Game:" ",
+    Rank:" ",
+    deta:[]
+    
+  }
+  changers=(eve)=>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.setState({[eve.target.name]:eve.target.value});
+    console.log(this.state)
+  }
+  btnaction=(act)=>{
+    act.preventDefault()
+const playersdata ={
+  Person:this.state.Person,
+  Game:this.state.Game,
+  Rank:this.state.Rank
 }
-
-export default App;
+const arr=this.state.deta;
+arr.push(playersdata)
+this.setState({deta:arr})
+  }
+  render() {
+    return (
+      <div id='body'>
+<h1 id='H1'> Welcome to sports Feed back form 2k23</h1>
+       
+       <form>
+       <div id='BLOGBOX'>
+        
+         <label htmlFor='name'>Name :</label>
+         <input type={"text"} placeholder='Name' name='Person' value={this.state.Person}  onChange={this.changers}></input>
+         <br />
+         <label htmlFor='Game'>Sports :</label>
+         <input type={"text"} placeholder='sports' name='Game' value={this.state.Game}  onChange={this.changers}></input>
+         <br />
+         <label htmlFor='Rank'>Rank :</label>
+         <input type={"number"} placeholder='rating' name='Rank' value={this.state.Rank} onChange={this.changers}></input>
+          <br/>
+          <button onClick={this.btnaction}>submit</button>
+          </div>
+          </form>
+         
+         <div id='continer'>
+        {  this.state.deta.map((value,index)=>{
+          return(
+            <div className='backlog' key={index}>Name : { value.Person}| Game: {value.Game}| Rank: {value.Rank}</div> 
+          )
+        })}
+         </div>
+      </div>
+    )
+  }
+};
